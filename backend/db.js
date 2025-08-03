b845191299ac227887be3434af3ed418a30b373b
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-08-02 21:36:09 -0500

    feat: :bug: rds
    change db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,16 @@
-const pool = new Pool({ 
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false }  //Agregar SSL obligatorio para RDS
+  ssl: { rejectUnauthorized: false } //Forzar SSL para RDS
 });
+
+module.exports = pool;
+
  
 
