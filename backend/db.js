60ffe7e412df8d7d59a902e93e8b5930b2fe9af3
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-08-02 23:13:51 -0500

    feat: :bug: change ec2
    correct ec2

--- backend/db.js
+++ backend/db.js
@@ -2,15 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  ssl: { rejectUnauthorized: false } //Forzar SSL para RDS
+  connectionString: `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}?sslmode=require`
 });
 
 module.exports = pool;
+
 
  
 
